ba0e8a79bc9d92017cbbd5ec3bb06363746e43ea
Bethany <bethany.hull@gmail.com>
2019-02-21 12:31:28 -0700

    account creation component and main style sheet
    Created an account creation component and built the basic html and added global styling to the main style sheet.

---
+++ front-end/src/app/account-creation/account-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-creation',
+  templateUrl: './account-creation.component.html',
+  styleUrls: ['./account-creation.component.css']
+})
+export class AccountCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AccountCreationComponent } from './account-creation/account-creation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AccountCreationComponent
   ],
   imports: [
     BrowserModule,
