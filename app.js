563603a1186d9f0981269b45e7fd1261434f64af
kacey <kacey.gam@gmail.com>
2019-03-06 12:14:03 -0700

    added name to readme, added dep script, added path /deploy to app.js

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'front-end', 'dist', 'front-end')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'front-end', 'dist', 'front-end','index.html'));
 });
-
+app.get('/deploy', (req, res) => {
+  console.log("Deploy!");
+  exec('./deployment-script.sh', (err, out) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(out);
+    }
+  });
+});
 app.listen(8080);
 
 module.exports = app;
