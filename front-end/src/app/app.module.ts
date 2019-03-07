8779d53fc4ca87a82f325cc9032537974aed0375
JaredRackley <jared.a.rackley@gmail.com>
2019-03-06 19:18:20 -0700

    stuff

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AccountCreationComponent } from './account-creation/account-creation.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes: Routes = [
+  
+  { path: 'home', component: LandingPageComponent },
+  { path: '', redirectTo:'/home', pathMatch:'full'}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AccountCreationComponent
+    AccountCreationComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
