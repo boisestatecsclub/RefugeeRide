9509eb305201d6500da603fc255304a82a49bc96
amussell <alexmussell@gmail.com>
2019-04-01 10:54:27 -0600

    Added backend test for Bethany

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser')
 
 var app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'front-end', 'dist', 'front-end')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'front-end', 'dist', 'front-end','index.html'));
 });
+
+app.post('/account/create/test', (req, res) => {
+  console.log(req.body)
+});
+
+
 app.listen(8080);
 
 module.exports = app;
